e4cff5d220f8aa9f22d12f7ee151eb985f571c6a
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-02-09 00:02:48 +0800

    rm txt

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,25 +62,6 @@ const Home = () => {
               publishing software like Aldus PageMaker including versions of
               Lorem Ipsum.
             </p>
-            <p>
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
-              It has roots in a piece of classical Latin literature from 45 BC,
-              making it over 2000 years old. Richard McClintock, a Latin
-              professor at Hampden-Sydney College in Virginia, looked up one of
-              the more obscure Latin words, consectetur, from a Lorem Ipsum
-              passage, and going through the cites of the word in classical
-              literature, discovered the undoubtable source. Lorem Ipsum comes
-              from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et
-              Malorum" (The Extremes of Good and Evil) by Cicero, written in 45
-              BC. This book is a treatise on the theory of ethics, very popular
-              during the Renaissance. The first line of Lorem Ipsum, "Lorem
-              ipsum dolor sit amet..", comes from a line in section 1.10.32. The
-              standard chunk of Lorem Ipsum used since the 1500s is reproduced
-              below for those interested. Sections 1.10.32 and 1.10.33 from "de
-              Finibus Bonorum et Malorum" by Cicero are also reproduced in their
-              exact original form, accompanied by English versions from the 1914
-              translation by H. Rackham.
-            </p>
           </div>
         </div>
         <div className="grid h-screen text-center place-items-center">
@@ -101,11 +82,7 @@ const Home = () => {
                   felis feugiat eget. Mauris rhoncus diam vel quam aliquet
                   tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing
                   elit. Vestibulum vel iaculis leo. Mauris elementum lacus nec
-                  tellus mattis semper sed et ante. In non elit sit amet magna
-                  tristique aliquam. Nulla quis pretium dui, ut tristique orci.
-                  Donec eget odio eu metus accumsan ullamcorper eu in enim.
-                  Curabitur sed velit pellentesque, placerat lacus nec, vehicula
-                  magna.
+                  tellus mattis semper sed et ante. In non elit sit amet
                 </p>
               </div>
               <div>
@@ -124,7 +101,7 @@ const Home = () => {
                 </p>
               </div>
               <div>
-                <Profile className="mx-auto mb-12https://slc-website-nine.vercel.app" />
+                <Profile className="mx-auto mb-12 " />
                 <p>
                   Cras dapibus tellus quis neque tincidunt, id auctor justo
                   tristique. Fusce diam lorem, tincidunt id urna a, laoreet
