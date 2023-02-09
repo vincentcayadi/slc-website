ee0f910c1deb2cecde3efea8d4d16c4dcbb74d31
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-02-09 22:19:08 +0800

    maybe i add modal for about us

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+import About from "../pages/about";
 export default function Footer() {
   return (
     /* footer for social media links*/
     <>
       <div className="flex flex-row items-center justify-center p-2 m-2 text-xl text-center ">
-        Instagram | Facebook | Twitter | LinkedIn
+        Instagram | <Link href="about"> About Us </Link> | Ebooklet
       </div>
     </>
   );
