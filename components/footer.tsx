12b6d5f6f212f235029f09ded88e908edf75c2c6
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-02-10 23:31:04 +0800

    cleaned footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ import Booklet from "../public/booklet.png";
 import Image from "next/image";
 export default function Footer() {
   return (
-    <footer className="p-4 mt-4 bg-gray-900 shadow md:px-6 md:py-8">
+    <footer className="p-4 mt-4 text-center bg-gray-900 shadow md:px-6 md:py-8">
       <div className="sm:flex sm:items-center sm:justify-between">
         <a href="/" className="flex items-center mb-4 sm:mb-0">
           <Image
@@ -19,14 +19,23 @@ export default function Footer() {
             28th Student Leader Convention
           </span>
         </a>
-        <ul className="flex flex-wrap items-center mb-4 text-base text-gray-300 sm:mb-0">
+        <ul className="flex flex-wrap items-center mb-4 font-semibold text-gray-300 sm:mb-0">
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
+            <a
+              href="#"
+              className="mr-4 duration-300 border-b-2 border-transparent hover:border-white md:mr-6"
+            >
               Instagram
             </a>
           </li>
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
+            <a className="mr-2 md:mr-6">|</a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="mr-4 duration-300 border-b-2 border-transparent hover:border-white md:mr-6"
+            >
               E-Booklet
             </a>
           </li>
