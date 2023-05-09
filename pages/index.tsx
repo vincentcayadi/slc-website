4f11f637666d1a9518ceea6e060ec86d344ba82d
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-05-09 22:31:17 +0800

    fixed and removed styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,23 +52,18 @@ const Home = () => {
         className="h-screen bg-center bg-no-repeat bg-cover "
         id="about"
       >
-        <div className="grid w-3/4 h-screen grid-cols-1 m-auto my-12 text-white md:grid-cols-2 lg:my-0 place-items-center">
+        <div className="grid w-3/4 h-screen grid-cols-1 m-auto text-white md:grid-cols-2 place-items-center">
           <div>
-            <h1 className="mt-12 text-3xl font-bold text-left text-white lg:mt-0">
+            <h1 className="text-3xl font-bold text-left text-white lg:mt-0">
               About Us
             </h1>
-            <hr className="my-4"></hr>
+            <hr className="my-2"></hr>
             <p className="text-sm text-left lg:text-lg">
               The Student Leaders Convention is an esteemed annual event
-              organised by{" "}
-              <Link href={"https://www.hci.edu.sg/"} className="underline ">
-                Hwa Chong Institution
-              </Link>
-              , aimed at providing a platform for delegates to discuss global
-              issues. It was initiated in 1994 as a local one-day event, but has
-              since grown into a three-day convention. SLC was founded by Aaron
-              Beng, a former student leader who is now Singapore's Chief of
-              Defence Force. Distinguished political leaders have attended the
+              organised by Hwa Chong Institution aimed at providing a platform
+              for delegates to discuss global issues. It was initiated in 1994
+              as a local one-day event, but has since grown into a three-day
+              convention. Distinguished political leaders have attended the
               convention, including Deputy Prime Minister Mr Tharman
               Shanmugaratnam and Senior Minister of State Dr Janil Puthucheary.
             </p>
@@ -76,43 +71,41 @@ const Home = () => {
         </div>
       </section>
       <section
-        className="grid w-3/4 h-screen m-auto my-12 lg:my-0 place-items-center"
+        className="grid w-3/4 h-screen m-auto place-items-center"
         id="theme"
       >
         <div className="grid gird-cols-1 lg:grid-cols-3">
           <div className="grid place-items-center">
             <Image src={SLC} alt="logo" className="w-32 lg:w-64" />
           </div>
-          <div className="my-4 lg:col-span-2 lg:my-0">
+          <div className="my-2 lg:col-span-2 lg:my-0">
             <h1 className="text-lg font-bold text-center lg:text-3xl lg:text-right text-slcOrange">
               Change Or Continuity:{" "}
               <span className="block lg:inline ">A False Dichotomy</span>
             </h1>
-            <hr className="my-4"></hr>
+            <hr className="my-2"></hr>
             <p className="text-sm text-center lg:text-right lg:text-lg">
               Society has always undergone change, and recent events like the
               Russo-Ukraine war and COVID-19 pandemic have shown us how
               vulnerable we can be and the importance to value what we have.
               These events have also created positive changes such as global
-              solidarity. As student leaders, we must get ready for a very
-              different future while safeguarding essential aspects of human
-              consciousness.
+              solidarity.
             </p>
           </div>
         </div>
       </section>
-      <section className="grid w-3/4 h-screen m-auto my-12 lg:my-0 place-items-center">
+      <section className="grid w-3/4 h-screen m-auto place-items-center">
         <div className="grid gird-cols-1 lg:grid-cols-2">
           <Image
             src={Guest}
             className="w-64 m-auto mx-auto align-middle rounded-full lg:mr-0 lg:order-last"
             alt="aaron-beng"
           />
-          <div className="m-auto my-4">
+          <div className="m-auto my-2 lg:my-0">
             <h1 className="text-xl font-bold text-center lg:text-3xl lg:text-left text-slcOrange">
               Our Guest Of Honor
             </h1>
-            <hr className="my-4"></hr>
+            <hr className="my-2"></hr>
             <p className="text-sm text-center lg:text-left lg:text-lg">
               RADM Beng, who chaired the first Student Leaders Convention, was a
               former student of Hwa Chong Institution where he completed his
@@ -121,15 +114,14 @@ const Home = () => {
               English & Science Competition. After finishing his studies at Hwa
               Chong Institution, RADM Beng was granted the President's
               Scholarship and the Singapore Armed Forces Overseas Scholarship to
-              pursue Engineering & Economics at Yale University. He successfully
-              completed his degree and was conferred with a Bachelor's of
-              Science.
+              pursue Engineering & Economics at Yale University. He was later
+              conferred with a Bachelor's of Science.
             </p>
           </div>
         </div>
       </section>
 
-      <section className="grid my-12 text-center lg:my-0 lg:h-screen place-items-center">
+      <section className="grid h-screen text-center place-items-center">
         <div>
           <h1 className="mb-2 text-3xl font-bold text-center lg:text-4xl text-slcOrange">
             Convention Dates
@@ -172,7 +164,7 @@ const Home = () => {
             </button>
           </div>
           <hr></hr>
-          <div className="mt-4">
+          <div className="my-2">
             {selectedTabDates === "firstDay" && (
               <div>
                 <h2 className="text-2xl font-bold">First Day</h2>
@@ -195,12 +187,13 @@ const Home = () => {
         </div>
       </section>
       <section className="grid my-12 text-center lg:my-0 lg:h-screen place-items-center">
-        <div className="hidden lg:block">
-          <div className="grid text-center place-items-center">
-            <h1 className="mb-8 text-4xl font-bold text-center text-slcOrange">
+        <div className="hidden lg:text-center lg:block lg:place-items-center">
+          <div className="">
+            <h1 className="mb-4 text-4xl font-bold text-center text-slcOrange">
               Organising Committee
             </h1>
-            <div className="grid grid-cols-4 gap-8">
+            <hr></hr>
+            <div className="grid grid-cols-4 gap-8 my-2">
               <button
                 className={`text-lg ${
                   selectedTab === "adminCommittee" ? "font-bold" : ""
@@ -234,7 +227,8 @@ const Home = () => {
                 Programmes Committee
               </button>
             </div>
-            <div className="mt-8">
+            <hr></hr>
+            <div className="my-2">
               {selectedTab === "adminCommittee" && (
                 <div>
                   <h2 className="text-2xl font-bold">Admin Committee</h2>
@@ -262,35 +256,35 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <section className="block lg:hidden">
-          <div className="grid text-center place-items-center">
-            <div>
-              <h1 className="mb-8 text-2xl font-bold text-center">
-                Organising Committe
-              </h1>
-              <div className="grid gap-8">
-                <div>
-                  <div>
-                    <OneTeam />
-                  </div>
-                  <p>From left to right</p>
-                </div>
-                <div>
-                  <OneTeam />
-                </div>
-                <p>From left to right</p>
-                <div>
-                  <OneTeam />
-                </div>
-                <p>From left to right</p>
+      </section>
+      <section className="block lg:hidden">
+        <div className="grid text-center place-items-center">
+          <div>
+            <h1 className="mb-8 text-3xl font-bold text-center text-slcOrange">
+              Organising Committe
+            </h1>
+            <div className="grid gap-8">
+              <div>
                 <div>
                   <OneTeam />
                 </div>
                 <p>From left to right</p>
               </div>
+              <div>
+                <OneTeam />
+              </div>
+              <p>From left to right</p>
+              <div>
+                <OneTeam />
+              </div>
+              <p>From left to right</p>
+              <div>
+                <OneTeam />
+              </div>
+              <p>From left to right</p>
             </div>
           </div>
-        </section>
+        </div>
       </section>
       <Footer />
     </>
