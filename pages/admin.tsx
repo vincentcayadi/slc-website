389599d84e61688abe3dc14423e0d35d2c49b6bc
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-05-21 00:18:59 +0800

    bruh

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -2,21 +2,22 @@ import { createClient } from "@supabase/supabase-js";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
-
-const Login = () => (
-  <section className="grid w-3/4 h-screen m-auto place-items-center">
-    <div>
-      <Auth
-        supabaseClient={supabase}
-        appearance={{ theme: ThemeSupa }}
-        providers={[]}
-      />
-    </div>
-  </section>
-);
+const Login = () => {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+  return (
+    <section className="grid w-3/4 h-screen m-auto place-items-center">
+      <div>
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+          providers={[]}
+        />
+      </div>
+    </section>
+  );
+};
 
 export default Login;
