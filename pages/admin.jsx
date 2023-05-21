3de8a9ba8fd7cfbda1c26375b8a63b422ea88cbe
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-05-21 14:03:21 +0800

    styling of admin panel

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+import "tailwindcss/tailwind.css";
 
 const Login = () => {
   const supabase = createClient(
@@ -40,20 +41,26 @@ const Login = () => {
   } else {
     return (
       <section className="w-3/4 m-auto">
-        <div className="my-4">
+        <header className="my-4">
           <h1 className="text-xl font-bold text-center lg:text-3xl lg:text-left">
             Admin Panel
           </h1>
           <p className="text-xs italic lg:text-sm">
             With Great Power Comes Great Responsibility
           </p>
-        </div>
-        <div>
-          <h1 className="text-lg font-bold text-center lg:text-2xl lg:text-left">
-            Select YF
-          </h1>
-          <label for="YF">Choose YF:</label>
-          <select name="yf" id="yfs">
+        </header>
+        <div className="my-4">
+          <label
+            htmlFor="YF"
+            className="block text-lg font-bold text-center lg:text-2xl lg:text-left"
+          >
+            Choose YF:
+          </label>
+          <select
+            name="yf"
+            id="yfs"
+            className="block w-full px-4 py-2 border rounded"
+          >
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -74,10 +81,27 @@ const Login = () => {
             <option value="18">18</option>
           </select>
         </div>
-        <div>
-          <h1 className="text-lg font-bold text-center lg:text-2xl lg:text-left">
+        <div className="my-4">
+          <label
+            htmlFor="points"
+            className="block text-lg font-bold text-center lg:text-2xl lg:text-left"
+          >
             Input Points
-          </h1>
+          </label>
+          <input
+            type="text"
+            id="points"
+            name="points"
+            className="w-full px-4 py-2 border rounded"
+          />
+        </div>
+        <div className="my-4">
+          <button
+            type="submit"
+            className="px-4 py-2 text-white bg-blue-500 rounded"
+          >
+            Submit
+          </button>
         </div>
       </section>
     );
