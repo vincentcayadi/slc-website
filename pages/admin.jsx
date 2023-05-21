862ee10f72f60ad2cb404f8ff8dc891d2d2513be
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-05-21 20:01:48 +0800

    admin still broken qurey lb data

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -1,111 +1,23 @@
-import { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-import "tailwindcss/tailwind.css";
 
 const Login = () => {
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   );
-
-  const [session, setSession] = useState(null);
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-
-    return () => subscription.unsubscribe();
-  }, [supabase.auth]);
-
-  if (!session) {
-    return (
-      <section className="grid w-3/4 h-screen m-auto place-items-center">
-        <div>
-          <Auth
-            supabaseClient={supabase}
-            appearance={{ theme: ThemeSupa }}
-            providers={[]}
-          />
-        </div>
-      </section>
-    );
-  } else {
-    return (
-      <section className="w-3/4 m-auto">
-        <header className="my-4">
-          <h1 className="text-xl font-bold text-left lg:text-3xl">
-            Admin Panel
-          </h1>
-          <p className="text-xs italic lg:text-sm">
-            With Great Power Comes Great Responsibility
-          </p>
-        </header>
-        <div className="my-4">
-          <label
-            htmlFor="YF"
-            className="block text-lg font-bold text-left lg:text-2xl"
-          >
-            Choose YF
-          </label>
-          <select
-            name="yf"
-            id="yfs"
-            className="block w-full px-4 py-2 border rounded"
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-            <option value="11">11</option>
-            <option value="12">12</option>
-            <option value="13">13</option>
-            <option value="14">14</option>
-            <option value="15">15</option>
-            <option value="16">16</option>
-            <option value="17">17</option>
-            <option value="18">18</option>
-          </select>
-        </div>
-        <div className="my-4">
-          <label
-            htmlFor="points"
-            className="block text-lg font-bold text-left lg:text-2xl"
-          >
-            Input Points
-          </label>
-          <input
-            type="text"
-            id="points"
-            name="points"
-            className="w-full px-4 py-2 border rounded"
-          />
-        </div>
-        <div className="my-4">
-          <button
-            type="submit"
-            className="px-4 py-2 text-white bg-blue-500 rounded"
-          >
-            Submit
-          </button>
-        </div>
-      </section>
-    );
-  }
+  return (
+    <section className="grid w-3/4 h-screen m-auto place-items-center">
+      <div>
+        <Auth
+          supabaseClient={supabase}
+          appearance={{ theme: ThemeSupa }}
+          providers={[]}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Login;
--- pages/leaderboard.jsx
+++ pages/leaderboard.jsx
@@ -1,9 +1,27 @@
-const leaderBoard = () => {
-  return (
-    <div>
-      <h1>Leaderboard</h1>
-    </div>
-  );
-};
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
 
-export default leaderBoard;
+export default function Leaderboard() {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+
+  const [score, setScore] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await supabase.from("yfpoints").select();
+      setScore(data);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return <pre>{JSON.stringify(score, null, 2)}</pre>;
+}
